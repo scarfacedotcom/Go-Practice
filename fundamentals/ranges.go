bcafd9fee368e62d307f9ff8018ad0d55039b257
scarfacedotcom <peteradaaku@gmail.com>
2023-04-13 13:20:25 +0100

    ranges

--- fundamentals/ranges.go
+++ fundamentals/ranges.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	slice := []string{"Hello", "world", "!"}
 	for i, element := range slice {
-		fmt.Println(i, element)
+		fmt.Println(i, element, ":")
+		for _, ch := range element {
+			fmt.Printf("  %q\n", ch)
+		}
+	}
+	number := []int{1, 2, 3, 4, 5}
+	for i, element := range number {
+		fmt.Printf("Index: %d, Value: %d\n", i, element)
 	}
 }
