348dc1e4da1d90b981f9f5bb5dd5b9d383eae2a4
scarfacedotcom <peteradaaku@gmail.com>
2023-04-25 17:19:53 +0100

    maps

--- fundamentals/map.go
+++ fundamentals/map.go
@@ -4,13 +4,31 @@ import "fmt"
 
 func main() {
 	shoppingList := make(map[string]int)
-	shoppingList["egss"] = 11
+	shoppingList["eggs"] = 11
+	shoppingList["bama"] = 10
 	shoppingList["tea"] = 3
 	shoppingList["milk"] += 1
-	shoppingList["egss"] += 1
+	shoppingList["eggs"] += 1
 
 	fmt.Println(shoppingList)
 	delete(shoppingList, "tea")
-	fmt.Println(shoppingList)
+	fmt.Println("Tea deleted, New List", shoppingList)
+	fmt.Println("need", shoppingList["eggs"], "eggs")
+	sugar, found := shoppingList["sugar"]
+	if !found {
+		fmt.Println("sugar not found in the map", sugar)
+	}
+	// bama, found := shoppingList["bama"]
+	// if !found {
+	// 	fmt.Println("no bama bro", bama)
+	// } else {
+	// 	fmt.Println("we currently have like", bama, "boxes of bama")
+	//}
+
+	totalItems := 0
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+	fmt.Println("There are", totalItems, "on the shopping list")
 
 }
