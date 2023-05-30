4d353a25e8120f07a8c91dbcb4fc50ee4790defe
scarfacedotcom <peteradaaku@gmail.com>
2023-05-30 06:01:16 +0100

    slices

--- fundamentals/slice3.go
+++ fundamentals/slice3.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
+type Part string
+
+func showLine(line []Part) {
+	for i := 0; i < len(line); i++ {
+		part := line[i]
+		fmt.Println(part)
+	}
+
+}
+
 func main() {
+	assemblyLine := make([]Part, 3)
+
+	assemblyLine[0] = "peter"
+	assemblyLine[1] = "scar"
+	assemblyLine[2] = "jay"
+
+	fmt.Println("------The first three parts-------")
+	showLine(assemblyLine)
+
+	assemblyLine = append(assemblyLine, "mum", "dad")
+	fmt.Println("------The New parts-------")
+	showLine(assemblyLine)
+
+	assemblyLine = assemblyLine[3:]
+	fmt.Println("-------Two new parts---------")
+	showLine(assemblyLine)
 
 }
