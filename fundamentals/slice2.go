942d3d610de8f83b3df9c7c932396a4887fdce5d
scarfacedotcom <peteradaaku@gmail.com>
2023-05-30 05:37:11 +0100

    slices

---
+++ fundamentals/slice2.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func printSlices(title string, slice []string) {
+	fmt.Println()
+	fmt.Println("------", title, "----------")
+	for i := 0; i < len(slice); i++ {
+		element := slice[i]
+		fmt.Println(element)
+	}
+
+}
+
+func main() {
+	route := []string{"maths", "biology", "chemistry"}
+	printSlices("Route 1", route)
+
+	route = append(route, "pharm")
+	printSlices("Route 2", route)
+
+	fmt.Println()
+	fmt.Println(route[0], "visited")
+	fmt.Println(route[1], "visited")
+
+	route = route[2:]
+	printSlices("Route 3", route)
+
+}
