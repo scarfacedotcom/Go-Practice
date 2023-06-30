8c41e93a57bb33f007978819f27869a9ce84d989
scarfacedotcom <peteradaaku@gmail.com>
2023-06-30 08:13:51 +0100

    packages

--- fundamentals/pkg/main/main.go
+++ fundamentals/pkg/main/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"coursecontent/fundamentals/pkg/display"
+	"coursecontent/fundamentals/pkg/msg"
+)
 
+func main() {
+	msg.Hi()
+	display.Display("hello from display")
+	msg.Exciting("an exciting message")
 }
